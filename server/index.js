d2c2f61a0071d03b57d91834675c04b29dfd6f33
Teepath <tundeadebanjo246@gmail.com>
2022-12-19 12:26:34 +0100

    cors2

--- server/index.js
+++ server/index.js
@@ -4,14 +4,12 @@ const cors = require("cors");
 const helmet= require("helmet");
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
-
+const app = express();
+app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}));
 app.use(cors());
 connectDB();
-const app = express();
-
-app.use(express.json());
 app.use('/api/products', productRoutes)
 
 const port = process.env.PORT || 5000;
