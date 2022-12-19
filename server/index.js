8fd5b1ac0ffd2a83565012613dc63c8973539d7a
Teepath <tundeadebanjo246@gmail.com>
2022-12-19 03:20:13 +0100

    package json

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const productRoutes = require("./routes/productRoutes");
 
 connectDB();
 const app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 app.use(express.json());
 app.use('/api/products', productRoutes)
 
